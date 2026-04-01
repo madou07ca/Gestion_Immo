026dd76d868f87e3d3d6238277ea7361fbc38b1d
madou07ca <mdoubobobarry07ca@gmail.com>
2026-04-02 00:34:58 +0200

    maj du 20260402

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h6l5ecvr2e8"
+    "revision": "0.vfc69l9engs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
